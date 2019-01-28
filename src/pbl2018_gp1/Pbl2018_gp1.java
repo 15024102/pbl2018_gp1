fd01292b95ffb8def7725f192c5f42f8ac5b5f78
10024006-pbl2018 <15024102@mmm.muroran-it.ac.jp>
2019-01-28 15:52:01 +0900

    テスト

--- src/pbl2018_gp1/Pbl2018_gp1.java
+++ src/pbl2018_gp1/Pbl2018_gp1.java
@@ -15,7 +15,7 @@ public class Pbl2018_gp1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        // TODO code application logic here
     }
     
 }
